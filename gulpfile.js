c06c84b4c0db8678c353cc0ee423c6b5c5444f4a
Filipp Nikonov <p.nikonov99@gmail.com>
2019-12-10 23:52:25 +0300

    add first and second section

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,5 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync');
-const gulp = require('gulp');
-const browserSync = require('browser-sync');
 const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
